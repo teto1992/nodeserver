7f753d2ec2b9a1a3489f03e1bc115ccf6e104304
AFx3 <waxnwaxofficial@gmail.com>
2023-06-09 09:12:53 +0200

    first commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3070;
 
 app.get('/', (req, res) => {
-  res.send("TODO: Write your first and last name here - Write your student number here: On your way to pass the lab test... focus on submitting the right files on time!"); // e.g. Mario Rossi - 123456
+  res.send("Andrea Franceschi- 665958: On your way to pass the lab test... focus on submitting the right files on time!"); // e.g. Mario Rossi - 123456
 });
 
 
